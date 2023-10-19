f18d5f3d0e500112bd710b7872755587145eae57
fahadkhanrakib <fhdrkb@gmail.com>
2023-10-19 15:20:51 +0600

    css changed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -9,82 +9,88 @@ import { AiOutlineClose } from "react-icons/ai";
 import { AiOutlineAppstoreAdd } from "react-icons/ai";
 
 function Header() {
-  const [toggle, setToggle] = useState(false);
-  return (
-    <div>
-      <header className="header">
-        <nav className="nav container">
-          <a href="index.html" className="nav__logo">
-            Fahad
-          </a>
-          <div className={`nav__menu ${toggle ? "show-menu" : ""}`}>
-            <ul className="nav__list cs_grid">
-              <li className="nav__item">
-                <a href="" className="nav__link active-link">
-                  <span className="nav__icon">
-                    <BiHomeAlt />
-                  </span>
+    const [toggle, setToggle] = useState(false);
+    return (
+        <div>
+            <header className="header">
+                <nav className="nav container">
+                    <a href="index.html" className="nav__logo">
+                        Fahad
+                    </a>
+                    <div className={`nav__menu ${toggle ? "show-menu" : ""}`}>
+                        <ul className="nav__list cs_grid">
+                            <li className="nav__item">
+                                <a href="" className="nav__link active-link">
+                                    <span className="nav__icon">
+                                        <BiHomeAlt />
+                                    </span>
 
-                  <span>Home</span>
-                </a>
-              </li>
-              <li className="nav__item">
-                <a href="" className="nav__link">
-                  <span className="nav__icon">
-                    <AiOutlineUser />
-                  </span>
+                                    <span>Home</span>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="" className="nav__link">
+                                    <span className="nav__icon">
+                                        <AiOutlineUser />
+                                    </span>
 
-                  <span>About</span>
-                </a>
-              </li>
-              <li className="nav__item">
-                <a href="" className="nav__link">
-                  <span className="nav__icon">
-                    <HiOutlineDocumentText />
-                  </span>
+                                    <span>About</span>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="" className="nav__link">
+                                    <span className="nav__icon">
+                                        <HiOutlineDocumentText />
+                                    </span>
 
-                  <span>Skill</span>
-                </a>
-              </li>
-              <li className="nav__item">
-                <a href="" className="nav__link">
-                  <span className="nav__icon">
-                    <BsBriefcase />
-                  </span>
+                                    <span>Skill</span>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="" className="nav__link">
+                                    <span className="nav__icon">
+                                        <BsBriefcase />
+                                    </span>
 
-                  <span>Service</span>
-                </a>
-              </li>
-              <li className="nav__item">
-                <a href="" className="nav__link">
-                  <span className="nav__icon">
-                    <RiGalleryLine />
-                  </span>
+                                    <span>Service</span>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="" className="nav__link">
+                                    <span className="nav__icon">
+                                        <RiGalleryLine />
+                                    </span>
 
-                  <span>Portfolio</span>
-                </a>
-              </li>
-              <li className="nav__item">
-                <a href="" className="nav__link">
-                  <span className="nav__icon">
-                    <LuContact />
-                  </span>
+                                    <span>Portfolio</span>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="" className="nav__link">
+                                    <span className="nav__icon">
+                                        <LuContact />
+                                    </span>
 
-                  <span>Contact</span>
-                </a>
-              </li>
-            </ul>
-            <span onClick={() => setToggle(!toggle)} class="nav__close">
-              <AiOutlineClose />
-            </span>
-          </div>
-          <div onClick={() => setToggle(!toggle)} className="nav__toggle">
-            <AiOutlineAppstoreAdd />
-          </div>
-        </nav>
-      </header>
-    </div>
-  );
+                                    <span>Contact</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <span
+                            onClick={() => setToggle(!toggle)}
+                            className="nav__close"
+                        >
+                            <AiOutlineClose />
+                        </span>
+                    </div>
+                    <div
+                        onClick={() => setToggle(!toggle)}
+                        className="nav__toggle"
+                    >
+                        <AiOutlineAppstoreAdd />
+                    </div>
+                </nav>
+            </header>
+        </div>
+    );
 }
 
 export default Header;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,86 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
+    content: ["./index.html", "./src/**/*.{js,jsx}"],
+    theme: {
+        extend: {
+            screens: {
+                sm: "992px"
+            },
+            spacing: {
+                headerHeight: "3rem",
+
+                //Margin Custom Css
+
+                0.25: "0.25rem",
+                0.5: "0.5rem",
+                0.75: "0.75rem",
+                1: "1rem",
+                1.5: "1.5rem",
+                2: "2rem",
+                2.5: "2.5rem",
+                3: "3rem"
+            },
+            colors: {
+                titleColor: "hsl(0,0%,20%)",
+                titleColorDark: "hsl(0,0%,0%)",
+                textColor: "hsl(0,0%,46%)",
+                bodyColor: "hsl(0,0%,98%)",
+                containerColor: "#fff"
+            },
+            fontSize: {
+                big: [
+                    "3.5rem",
+                    {
+                        sm: "2.75rem"
+                    }
+                ],
+                h1: [
+                    "2.25rem",
+                    {
+                        sm: "1.5rem"
+                    }
+                ],
+                h2: [
+                    "1.5rem",
+                    {
+                        sm: "1.25rem"
+                    }
+                ],
+                h3: [
+                    "1.25rem",
+                    {
+                        sm: "1rem"
+                    }
+                ],
+                normal: [
+                    "1rem",
+                    {
+                        sm: "0.938rem"
+                    }
+                ],
+                small: [
+                    "0.875rem",
+                    {
+                        sm: "0.813rem"
+                    }
+                ],
+                smaller: [
+                    "0.813rem",
+                    {
+                        sm: "0.75rem"
+                    }
+                ],
+                tiny: "0.625rem"
+            },
+            fontWeight: {
+                normal: 400,
+                medium: 500,
+                semiBold: 600
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"]
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
